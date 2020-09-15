70b42e830070a9dca350cca327efdd535c14638d
marzione00 <marzio.decorato@outlook.it>
2020-09-15 16:11:39 +0200

    pub

--- Script.R
+++ Script.R
@@ -36,12 +36,41 @@ tree.planet <- rpart(P_H~P_P+S_T+P_D+P_PN+P_A+P_D_E+P_F+P_T_E+S_R_E+S_L+P_R+P_M,
 
 rfor.planet <-randomForest(as.factor(P_H)~P_P+S_T+P_D+P_PN+P_A+P_D_E+P_F+P_T_E+S_R_E+S_L+P_R+P_M,data=Planets_dataset, subset=Planets_dataset_train,localImp = TRUE,importance=TRUE,proximity=TRUE)
 
+pca.train<-Planets_dataset[Planets_dataset_train,]
+pca.test<-Planets_dataset[-Planets_dataset_train,]
+pca.planet <- prcomp(pca.train[,2:14], center = TRUE,scale. = TRUE)
+pca.planet.test  <-  predict(pca.planet, pca.test[,2:14])
+#pca.planet.test <- prcomp(pca.test[,2:14], center = TRUE,scale. = TRUE)
+autoplot(pca.planet,data=pca.train[,2:14],col="P_H")
+autoplot(pca.planet.test)
 
-pca.planet <- prcomp(Planets_dataset[,2:14], center = TRUE,scale. = TRUE)
+pca_out<-data.frame(pca.planet[["x"]])
+pca_out_test<-data.frame(pca.planet.test[["x"]])
 
-#svm.planet <- svm( data=Planets_dataset, subset=Planets_dataset_train, kernel="radial")
-#summary(svm.planet)
-#plot(svm.planet,mode="pca")
+train<-pca_out[1:2]
+test<-pca_out_test[1:2]
+train["H"]<-pca.train[,12]
+test["H"]<-pca.test[,12]
+
+#pca.p <- preProcess(data.frame(Planets_dataset[,2:14]), method='pca',pcaComp = 2,method = c("center", "scale"))
+#autoplot(pca.p)
+#peppo<- predict(pca.p,"P_H")
+
+
+#svm_train<-pca_out[,1:2]
+
+
+svm.planet <- svm(H~., data=train,type = 'C-classification', kernel="linear")
+summary(svm.planet)
+plot(svm.planet,train)
+
+
+
+
+
+
+
+
 
 fancyRpartPlot(tree.planet,sub = "Planets Habitability", palettes = "OrRd")
 
@@ -60,12 +89,23 @@ print(importance(rfor.planet,type=2))
 
 rpart.plot(tree.planet,box.palette=c("red", "green"),digits=4,extra=106)
 
+svm.predict<-data.frame(predict(svm.planet,pca.planet.test[,1:2],type = "class"))
+colnames(svm.predict)[1]<-"H"
+
+svm.predict["T"]<-as.factor(pca.test[,12])
+
+
+pippo<-table()
 
 tree.predict<-predict(tree.planet, Planets_dataset_test, type = "class")
 rfor.predict<-predict(rfor.planet, Planets_dataset_test, type = "class")
 caret::confusionMatrix(table(tree.predict,Planets_dataset_test[,12]))
 caret::confusionMatrix(table(rfor.predict,Planets_dataset_test[,12]))
 
+tab <- rbind(table(svm.predict["H"]), table(svm.predict["T"]))
+
+
+caret::confusionMatrix(svm.predict["H"],svm.predict["H"])
 
 
 
@@ -79,46 +119,4 @@ caret::confusionMatrix(table(rfor.predict,Planets_dataset_test[,12]))
 
 
 
-
-
-
-
-
-
-
-
-Planets_dataset <- data.frame(read_excel("C:/Users/Marzio/Desktop/Planets/phl_exoplanet_catalogR.xlsx"))
-Planets_dataset_test_SET <- data.frame(read_excel("C:/Users/Marzio/Desktop/Planets/phl_exoplanet_catalog_test.xlsx"))
-
-set.seed(1)
-
-
-#caret.control <- trainControl(method = "cv",number=4)
-
-#rpart.cv <- train(as.factor(P_HABITABLE)~P_DISTANCE+P_PERIASTRON+P_APASTRON+P_DISTANCE_EFF+P_FLUX+P_TEMP_EQUIL+S_RADIUS_EST+S_LUMINOSITY, data = Planets_dataset,method = "rpart",trControl = caret.control)
-rpart.cv <- train(as.factor(P_HABITABLE)~P_DISTANCE+P_PERIASTRON+P_APASTRON+P_DISTANCE_EFF+P_FLUX+P_TEMP_EQUIL+S_RADIUS_EST+S_LUMINOSITY, data = Planets_dataset,method = "rpart")
-
-
-#rpart.best <- rpart.cv$finalModel
-
-prp(rpart.best, type = 5, extra = 1, under = TRUE)
-
-
-tree.predict<-predict(rpart.cv,Planets_dataset_test_SET)
-peppo<-data.frame(tree.predict)
-confusionMatrix(table(tree.predict,Planets_dataset_test_SET[,12]))
-
-
-
-
-
-
-
-
-
-
-
-
-palette = colorRampPalette(c("green", "blue", "red")) (20)
-heatmap(x = cor(Planets_dataset[,2:15]), col = palette, symm = TRUE, margins = c(10, 10), main = 'Planets',dist(Planets_dataset[,3:15],method = 'euclidean'))
 
